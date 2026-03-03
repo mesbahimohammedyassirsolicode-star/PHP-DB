8626207c36e01869ca30f8da8e0af5ce74cf708e
mesbahi mohammed yassir <mesbahi.mohammedyassir.solicode@gmail.com>
2026-03-03 12:43:39 +0000

    Implement prepared queries for user insertion
    Added a prepared query using bindParam for user insertion.

--- 📘 Chapter 5 – Prepared Queries and Related Parameters/utilisateur_secure.php
+++ 📘 Chapter 5 – Prepared Queries and Related Parameters/utilisateur_secure.php
@@ -1,12 +1,20 @@
 <?php
 require 'connexion.php';
-
+//Prepared query with named parameters
 $stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, email) VALUES (:nom, :email)");
 $stmt->execute([
     'nom' => 'Alice',
     'email' => 'alice@test.com'
 ]);
 echo "Utilisateur ajouté.";
+//Prepared query with bindParam
+$nom = 'Bob';
+$email = 'bob@test.com';
+$stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, email) VALUES (:nom, :email)");
+$stmt->bindParam(':nom', $nom);
+$stmt->bindParam(':email', $email);
+$stmt->execute();
+
 // safe query 
 $stmt = $pdo->prepare("SELECT * FROM Utilisateur WHERE email = :email");
 $stmt->execute(['email' => 'alice@test.com']);
